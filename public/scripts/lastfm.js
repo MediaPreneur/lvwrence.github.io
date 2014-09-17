15a412c11800b34bd8f0ea99c813d33f9134912a
Lawrence Wu <lawrence@lawrencewu.me>
2014-09-17 00:58:50 -0400

    fix style

--- public/scripts/lastfm.js
+++ public/scripts/lastfm.js
@@ -1,4 +1,4 @@
-atomic.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Crook--&api_key=e2374a71d02582d469e0604eb87ace84&format=json').success(function (data, xhr) {
+atomic.get("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Crook--&api_key=e2374a71d02582d469e0604eb87ace84&format=json").success(function (data, xhr) {
   var mostRecentTrack = data["recenttracks"]["track"][0];
   if (mostRecentTrack["@attr"]["nowplaying"] == "true") {
     var name = mostRecentTrack["name"];
