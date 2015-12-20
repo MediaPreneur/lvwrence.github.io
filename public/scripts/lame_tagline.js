3172df15fba122fd110bb9b0f0d0c2bf97244356
Lawrence Wu <lawrence@lawrencewu.me>
2015-12-20 11:34:36 -0500

    add heap tracking and add more taglines

--- public/scripts/lame_tagline.js
+++ public/scripts/lame_tagline.js
@@ -1,7 +1,12 @@
-var terribleVariableName = [
-  '((λ (x) (rot13 x)) "ynjerapr")',
-  '43°41\'22.6" N, 79°17\'45.1" W',
+var possibleTaglines = [
+  '((λ (x) (rot13 x)) "ynjerapr")', // outputs "lawrence"
+  '43°41\'22.6" N, 79°17\'45.1" W', // the danforth
+  '43°39\'34.6" N, 79°23\'50.4" W', // bahen
+  '37°47\'12.1" N, 122°23\'59.4" W', // yelp
+  '∞ 🃏', // infinite jest
+  '¯\\_(ツ)_/¯', // lel
+  '(๑╹ڡ╹)╭ ～ ♡'
 ];
-var p = terribleVariableName[Math.floor(Math.random() * terribleVariableName.length)];
+var tagline = possibleTaglines[Math.floor(Math.random() * possibleTaglines.length)];
 
-document.getElementsByClassName("lead")[0].innerHTML = p;
+document.getElementsByClassName("lead")[0].innerHTML = tagline;
