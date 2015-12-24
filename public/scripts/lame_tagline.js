5f3bcbe9056dbe4e1566aa8a2fdbde9ff654a50e
Lawrence Wu <lawrence@lawrencewu.me>
2015-12-23 22:30:30 -0500

    update some taglines

--- public/scripts/lame_tagline.js
+++ public/scripts/lame_tagline.js
@@ -3,9 +3,8 @@ var possibleTaglines = [
   '43°41\'22.6" N, 79°17\'45.1" W', // the danforth
   '43°39\'34.6" N, 79°23\'50.4" W', // bahen
   '37°47\'12.1" N, 122°23\'59.4" W', // yelp
-  '∞ 🃏', // infinite jest
-  '¯\\_(ツ)_/¯', // lel
-  '(๑╹ڡ╹)╭ ～ ♡'
+  '{"status": "200 OK", "data": {"name": "lawrence", "age": 21, "location": "toronto"}}',
+  '(◕ᴥ◕)'
 ];
 var tagline = possibleTaglines[Math.floor(Math.random() * possibleTaglines.length)];
 
