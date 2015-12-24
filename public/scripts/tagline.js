9791e70415f7b5e3f4ba642e6c0c32d4d0050e4c
Lawrence Wu <lawrence@lawrencewu.me>
2015-12-24 13:46:56 -0500

    add tagline

--- public/scripts/tagline.js
+++ public/scripts/tagline.js
@@ -1,10 +1,11 @@
 var possibleTaglines = [
-  '((λ (x) (rot13 x)) "ynjerapr")', // outputs "lawrence"
-  '43°41\'22.6" N, 79°17\'45.1" W', // the danforth
-  '43°39\'34.6" N, 79°23\'50.4" W', // bahen
-  '37°47\'12.1" N, 122°23\'59.4" W', // yelp
+  '((λ (x) (rot13 x)) "ynjerapr")',
+  '43°41\'22.6" N, 79°17\'45.1" W',
+  '43°39\'34.6" N, 79°23\'50.4" W',
+  '37°47\'12.1" N, 122°23\'59.4" W',
   '{"status": "200 OK", "data": {"name": "lawrence", "age": 21, "location": "toronto"}}',
-  '(◕ᴥ◕)'
+  '(◕ᴥ◕)',
+  '\'lawrence\':: [Char]'
 ];
 var tagline = possibleTaglines[Math.floor(Math.random() * possibleTaglines.length)];
 
