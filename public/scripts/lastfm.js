c61fa3d0478f552a4445b11f6bfac982a7b4bb2e
Lawrence Wu <lawrence@lawrencewu.me>
2015-12-23 23:13:13 -0500

    wrap some stuff in a try/catch

--- public/scripts/lastfm.js
+++ public/scripts/lastfm.js
@@ -1,9 +1,12 @@
 atomic.get("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Crook--&api_key=e2374a71d02582d469e0604eb87ace84&format=json").success(function (data, xhr) {
-  var mostRecentTrack = data["recenttracks"]["track"][0];
-  if (mostRecentTrack["@attr"]["nowplaying"] == "true") {
-    var name = mostRecentTrack["name"];
-    var artist = mostRecentTrack["artist"]["#text"];
+  try {
+    var mostRecentTrack = data["recenttracks"]["track"][0];
+    if (mostRecentTrack["@attr"]["nowplaying"] == "true") {
+      var name = mostRecentTrack["name"];
+      var artist = mostRecentTrack["artist"]["#text"];
 
-    document.getElementById("currently-playing").innerHTML = "currently listening to " + name + " by " + artist;
+      document.getElementById("currently-playing").innerHTML = "currently listening to " + name + " by " + artist;
+    }
+  } catch (e) {
   }
-})
+});
