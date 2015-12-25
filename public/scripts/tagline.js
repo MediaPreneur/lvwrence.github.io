45bd1f8283363cb2089044cc90d701491b3dccc7
Lawrence Wu <lawrence@lawrencewu.me>
2015-12-25 13:22:43 -0500

    add some hello worlds

--- public/scripts/tagline.js
+++ public/scripts/tagline.js
@@ -3,9 +3,14 @@ var possibleTaglines = [
   '43°41\'22.6" N, 79°17\'45.1" W',
   '43°39\'34.6" N, 79°23\'50.4" W',
   '37°47\'12.1" N, 122°23\'59.4" W',
-  '{"status": "200 OK", "data": {"name": "lawrence", "age": 21, "location": "toronto"}}',
+  '{"status": "200 OK", "data": {"name": "lawrence", "age": 21, "location": "montreal"}}',
   '(◕ᴥ◕)',
-  '"lawrence":: [Char]'
+  '"hello world":: [Char]',
+  'console.log("hello world");',
+  'printf("hello world\\n");',
+  'std::cout << "hello world" << std::endl;',
+  'print("hello world")',
+  'NSLog("hello world");'
 ];
 var tagline = possibleTaglines[Math.floor(Math.random() * possibleTaglines.length)];
 
