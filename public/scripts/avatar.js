f08edae5e3e5c92309132f24bb1f2affbfbf5c13
Lawrence Wu <lawrence@lawrencewu.me>
2016-03-12 21:07:14 -0800

    make it so no identical consecutive images

--- public/scripts/avatar.js
+++ public/scripts/avatar.js
@@ -3,7 +3,25 @@ var possibleImages = [
   'av2.jpg',
   'av3.jpg',
   'av4.jpg',
+  'av5.jpg',
+  'av6.jpg',
+  'av7.jpg',
 ];
+
+// make sure we don't see the same av consecutively
+if (localStorage.getItem('av')) {
+  var av = localStorage.getItem('av');
+  var i = possibleImages.indexOf(av);
+  if (i > -1) {
+    possibleImages.splice(i, 1);
+  }
+}
+
+// choose an avatar
 var image = possibleImages[Math.floor(Math.random() * possibleImages.length)];
 
+// set DOM to chosen av
 document.getElementById("av").src = "/public/images/" + image;
+
+// save current av to localstorage
+localStorage.setItem('av', image);
