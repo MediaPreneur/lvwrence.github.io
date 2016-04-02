839aef89e56974a4a050080648236c2a708b3bbc
Lawrence Wu <lawrence@lawrencewu.me>
2016-04-01 23:22:02 -0700

    add some muhhfuckin CHARTS

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,1 +1,40 @@
-console.log('hi');
+d3.json("http://162.243.145.24/dashboard", function(err, data) {
+  if (err) {
+    console.warn(err);
+  }
+
+  renderCodeGraph(data["code"]);
+})
+
+function renderCodeGraph(data) {
+  var timeInHours = data["past_week"].map(function(seconds) {
+    var d = moment.duration(seconds, "seconds");
+    return d.asHours()
+  });
+
+  var loggedCodingData = [
+    {
+      x: [
+        moment().subtract(6, "days").format("dddd"),
+        moment().subtract(5, "days").format("dddd"),
+        moment().subtract(4, "days").format("dddd"),
+        moment().subtract(3, "days").format("dddd"),
+        moment().subtract(2, "days").format("dddd"),
+        moment().subtract(1, "days").format("dddd"),
+        moment().format("dddd")
+      ],
+      y: timeInHours,
+      type: "scatter"
+    }
+  ];
+  var layout = {
+    title: "Logged Time",
+    font: {
+    },
+    yaxis: {
+      title: "hours"
+    }
+  };
+
+  Plotly.newPlot("codeChart", loggedCodingData, layout, {displayModeBar: false, staticPlot: true});
+}
