69f1013f709bb7f376301da753c70caa121846e9
Lawrence Wu <lawrence@lawrencewu.me>
2016-04-05 23:12:38 -0700

    revamp dashboard infra

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,12 +1,23 @@
-d3.json("http://162.243.145.24/dashboard", function(err, data) {
-  if (err) {
-    console.warn(err);
+requirejs.config({
+  paths: {
+    "chartjs": "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.1.0/Chart.min",
+    "momentjs": "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/moment.min"
   }
-  console.log(data);
+});
 
-  renderCodeGraph(data["coding"]);
-  renderLiftTable(data["lifting"]);
-})
+var moment;
+require(["chartjs", "momentjs"], function(Chart, momentjs) {
+  moment = momentjs;
+  atomic
+    .get("http://162.243.145.24/dashboard")
+    .success(function(data, xhr) {
+      renderCodeGraph(data["coding"]);
+    })
+    .error(function(data, xhr) {
+      console.log(data);
+    });
+});
+
 
 function renderCodeGraph(data) {
   var timeInHours = data["past_week"].map(function(seconds) {
@@ -14,32 +25,30 @@ function renderCodeGraph(data) {
     return d.asHours()
   });
 
-  var loggedCodingData = [
-    {
-      x: [
-        moment().subtract(6, "days").format("dddd"),
-        moment().subtract(5, "days").format("dddd"),
-        moment().subtract(4, "days").format("dddd"),
-        moment().subtract(3, "days").format("dddd"),
-        moment().subtract(2, "days").format("dddd"),
-        moment().subtract(1, "days").format("dddd"),
-        moment().format("dddd")
-      ],
-      y: timeInHours,
-      type: "scatter"
-    }
-  ];
-  var layout = {
-    title: "Logged Time",
-    font: {
-    },
-    yaxis: {
-      title: "hours"
-    }
+  labels = [
+    moment().subtract(6, "days").format("dddd"),
+    moment().subtract(5, "days").format("dddd"),
+    moment().subtract(4, "days").format("dddd"),
+    moment().subtract(3, "days").format("dddd"),
+    moment().subtract(2, "days").format("dddd"),
+    moment().subtract(1, "days").format("dddd"),
+    moment().format("dddd")
+  ]
+
+  var data = {
+    labels: labels,
+    datasets: [
+			{
+				label: "My First dataset",
+				fillColor: "rgba(220,220,220,0.5)",
+				strokeColor: "rgba(220,220,220,0.8)",
+				highlightFill: "rgba(220,220,220,0.75)",
+				highlightStroke: "rgba(220,220,220,1)",
+        data: timeInHours
+      }
+    ]
   };
 
-  Plotly.newPlot("codeChart", loggedCodingData, layout, {displayModeBar: false, staticPlot: true});
-}
-
-function renderLiftTable(data) {
+  var ctx = document.getElementById("codeLog").getContext("2d");
+  var codeChart = new Chart(ctx).Bar(data)
 }
