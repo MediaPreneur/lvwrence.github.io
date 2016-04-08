3593762af42111168fab8bad9bd6e11060461ad1
Lawrence Wu <lawrence@lawrencewu.me>
2016-04-08 00:17:40 -0700

    add bar graph (needs positioning)

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,19 +1,20 @@
 requirejs.config({
   paths: {
-    "momentjs": "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/moment.min",
-    "d3": "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.16/d3.min",
-    "superagent": "https://cdnjs.cloudflare.com/ajax/libs/superagent/1.2.0/superagent.min"
+    momentjs: "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/moment.min",
+    d3: "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.16/d3.min",
+    dimple: "http://dimplejs.org/dist/dimple.v2.1.6.min",
+    superagent: "https://cdnjs.cloudflare.com/ajax/libs/superagent/1.2.0/superagent.min"
   }
 });
 
 var moment;
-require(["momentjs", "d3", "superagent"], function(momentjs, d3, request) {
+require(["momentjs", "d3", "superagent", "dimple"], function(momentjs, d3, request, dimple) {
   moment = momentjs;
   request
   .get("http://162.243.145.24/dashboard")
   .end(function(err, res) {
     renderDailyAverage(res.body);
-    // renderCodeGraph(data["coding"]);
+    renderLoggedTime(res.body);
   });
 });
 
@@ -24,7 +25,22 @@ function renderDailyAverage(data) {
   document.getElementById("daily-average").innerHTML = innerHTML;
 }
 
-function renderCodeGraph(data) {
-  console.log(data);
-  d3.select("#code").append("h1");
+function renderLoggedTime(data) {
+  var loggedTime = data['coding']['past_week'];
+  // massage the data a lil bit here
+  loggedTime = loggedTime.map(function(obj) {
+    return {
+      day: obj['date'],
+      hours: moment.duration(obj['total_seconds'], 'seconds').asHours()
+    }
+  });
+	var svg = dimple.newSvg("#logged-time", 500, 400);
+  var loggedTimeChart = new dimple.chart(svg, loggedTime);
+  loggedTimeChart.setBounds(60, 30, 510, 305)
+  var x = loggedTimeChart.addTimeAxis("x", "day", "%Y-%m-%d", "%a");
+  var y = loggedTimeChart.addMeasureAxis("y", "hours");
+  y.title = "Hours";
+  x.floatingBarWidth = 40;
+  loggedTimeChart.addSeries(null, dimple.plot.bar);
+  loggedTimeChart.draw(2000);
 }
