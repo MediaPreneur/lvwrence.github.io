f6803907825d5f41680be645c0fee2a74b375de1
Lawrence Wu <lawrence@lawrencewu.me>
2016-04-10 16:16:41 -0700

    add interactive age to tagline

--- public/scripts/tagline.js
+++ public/scripts/tagline.js
@@ -1,9 +1,11 @@
+var SPECIAL_INTERACTIVE_TAGLINE = 'SPECIAL_INTERACTIVE_TAGLINE';
+
 var possibleTaglines = [
+  SPECIAL_INTERACTIVE_TAGLINE,
   '((λ (x) (rot13 x)) "ynjerapr")',
   '43°41\'22.6" N, 79°17\'45.1" W',
   '43°39\'34.6" N, 79°23\'50.4" W',
   '37°47\'12.1" N, 122°23\'59.4" W',
-  '{"status": "200 OK", "data": {"name": "lawrence", "age": 21, "location": "montreal"}}',
   '(◕ᴥ◕)',
   '"hello world":: [Char]',
   'console.log("hello world");',
@@ -25,8 +27,22 @@ if (localStorage.getItem('tagline')) {
 // choose a tagline
 var tagline = possibleTaglines[Math.floor(Math.random() * possibleTaglines.length)];
 
-// set DOM to chosen tagline
-document.getElementsByClassName("lead")[0].innerHTML = tagline;
+// set DOM to chosen tagline but do special logic if it's the SPECIAL one
+if (tagline === SPECIAL_INTERACTIVE_TAGLINE) {
+  function renderCurrentAgeInTagline() {
+    var now = Date.now();
+    var dob = 781315200000;
+    var diff = now - dob;
+    var age = diff / 1000 / 60 / 60 / 24 / 365;
+    var fixedAge = age.toFixed(10);
+    // do some special rendering logic
+    var specialTagline = '{"status": "200 OK", "data": {"name": "lawrence", "age": ' + fixedAge + ', "location": "montreal"}}';
+    document.getElementsByClassName("lead")[0].innerHTML = specialTagline;
+  }
+  setInterval(renderCurrentAgeInTagline, 200);
+  } else {
+  document.getElementsByClassName("lead")[0].innerHTML = tagline;
+}
 
 // save current tagline to localstorage
 localStorage.setItem('tagline', tagline);
