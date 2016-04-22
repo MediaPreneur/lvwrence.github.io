47dc69e000b6501b16e06b8dbe038d7529ddd8d2
Lawrence Wu <lawrence@lawrencewu.me>
2016-04-21 18:43:31 -0700

    add coachella blog post

---
+++ _posts/2016-4-20-my-heartrate-at-coachella.md
@@ -0,0 +1,14 @@
+---
+layout: post
+title: My Heartrate at Coachella
+---
+
+I wore my Fitbit to Coachella. Here's the data I scraped from it afterwards.
+
+<div id="friday" style="width: 100%; height: 500px;"></div>
+<div id="saturday" style="width: 100%; height: 500px;"></div>
+<div id="sunday" style="width: 100%; height: 500px;"></div>
+
+<script data-main="/public/scripts/coachella"
+        src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.2.0/require.min.js"
+        defer></script>
---
+++ public/scripts/coachella.js
@@ -0,0 +1,122 @@
+requirejs.config({
+	paths: {
+    superagent: "https://cdnjs.cloudflare.com/ajax/libs/superagent/1.2.0/superagent.min",
+    d3: "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.16/d3.min",
+    c3: "https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min"
+	},
+});
+
+require(["superagent", "d3", "c3"], function(request, d3, c3) {
+  request
+  .get("https://raw.githubusercontent.com/lvwrence/coachella/master/friday.json")
+  .end(function(err, res) {
+    var body = JSON.parse(res.text);
+    var data = body['activities-heart-intraday']['dataset'];
+    var x = data.map(function(obj) {
+      return obj['time'];
+    });
+
+    var hr = data.map(function(obj) {
+      return obj['value'];
+    });
+
+    c3.generate({
+      bindto: '#friday',
+      data: {
+        x: 'x',
+        xFormat: '%H:%M:%S',
+        columns: [
+          ['x'].concat(x),
+          ['heartrate'].concat(hr),
+        ]
+      },
+      point: {
+        show: false
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%H:%M:%S'
+          }
+        }
+      }
+    });
+  });
+
+
+  request
+  .get("https://raw.githubusercontent.com/lvwrence/coachella/master/saturday.json")
+  .end(function(err, res) {
+    var body = JSON.parse(res.text);
+    var data = body['activities-heart-intraday']['dataset'];
+    var x = data.map(function(obj) {
+      return obj['time'];
+    });
+
+    var hr = data.map(function(obj) {
+      return obj['value'];
+    });
+
+    c3.generate({
+      bindto: '#saturday',
+      data: {
+        x: 'x',
+        xFormat: '%H:%M:%S',
+        columns: [
+          ['x'].concat(x),
+          ['heartrate'].concat(hr),
+        ]
+      },
+      point: {
+        show: false
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%H:%M:%S'
+          }
+        }
+      }
+    });
+  });
+
+
+  request
+  .get("https://raw.githubusercontent.com/lvwrence/coachella/master/sunday.json")
+  .end(function(err, res) {
+    var body = JSON.parse(res.text);
+    var data = body['activities-heart-intraday']['dataset'];
+    var x = data.map(function(obj) {
+      return obj['time'];
+    });
+
+    var hr = data.map(function(obj) {
+      return obj['value'];
+    });
+
+    c3.generate({
+      bindto: '#sunday',
+      data: {
+        x: 'x',
+        xFormat: '%H:%M:%S',
+        columns: [
+          ['x'].concat(x),
+          ['heartrate'].concat(hr),
+        ]
+      },
+      point: {
+        show: false
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%H:%M:%S'
+          }
+        }
+      }
+    });
+  });
+});
