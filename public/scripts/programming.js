44e94002ea313262420e375ccdf9b15b9187b5be
Lawrence Wu <lawrence@lawrencewu.me>
2016-04-27 16:32:51 -0700

    add all charts

--- public/scripts/programming.js
+++ public/scripts/programming.js
@@ -34,6 +34,7 @@ define(function() {
 				labels: labels,
 				datasets: [
 					{
+            type: 'bar',
 						data: hours,
 						borderWidth: 1,
 						backgroundColor: "#6baed6",
@@ -74,9 +75,141 @@ define(function() {
 			});
     }
 
+    function renderCommitsMade(data) {
+      var commits = data['coding']['commits_for_past_week'];
+
+      // massage the data a lil bit here
+      var data = _.map(commits, function(obj) {
+        var date = new Date(obj['date']).toLocaleDateString();
+        return {
+          'date': date,
+          'commits': obj['num_commits']
+        }
+      });
+
+      var labels = _.map(data, 'date');
+      var commits = _.map(data, 'commits');
+
+			var data = {
+				labels: labels,
+				datasets: [
+					{
+						data: commits,
+						borderWidth: 1,
+						backgroundColor: "#6baed6",
+						borderColor: "#3182bd",
+						hoverBackgroundColor: "#3182bd",
+						hoverBorderColor: "#3182bd",
+					}
+				]
+			};
+
+			var ctx = document.getElementById("commits-made");
+      var myChart = new Chart(ctx, {
+				type: 'bar',
+				data: data,
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Commits'
+              }
+            }]
+          },
+          tooltips: {
+            callbacks: {
+              label: function(tooltipItem, data) {
+                return tooltipItem['yLabel'] + ' commits';
+              }
+            }
+          }
+        }
+			});
+    }
+
+    function renderLinesChanged(data) {
+      var commits = data['coding']['commits_for_past_week'];
+
+      // massage the data a lil bit here
+      var data = _.map(commits, function(obj) {
+        var date = new Date(obj['date']).toLocaleDateString();
+        return {
+          'date': date,
+          'added': obj['lines_added'],
+          'removed': obj['lines_removed']
+        }
+      });
+
+      var labels = _.map(data, 'date');
+      var added = _.map(data, 'added');
+      var removed = _.map(data, 'removed');
+
+			var data = {
+				labels: labels,
+				datasets: [
+					{
+            data: added,
+						borderWidth: 1,
+						backgroundColor: "#6baed6",
+						borderColor: "#3182bd",
+						hoverBackgroundColor: "#3182bd",
+						hoverBorderColor: "#3182bd",
+					},
+          {
+            data: removed,
+						borderWidth: 1,
+						backgroundColor: "#6baed6",
+						borderColor: "#3182bd",
+						hoverBackgroundColor: "#3182bd",
+						hoverBorderColor: "#3182bd",
+					}
+
+
+				]
+			};
+
+			var ctx = document.getElementById("lines-changed");
+      var myChart = new Chart(ctx, {
+				type: 'bar',
+				data: data,
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Lines'
+              }
+            }]
+          },
+          tooltips: {
+            callbacks: {
+              label: function(tooltipItem, data) {
+                var hours = tooltipItem['yLabel'];
+                var duration = moment.duration(hours, 'hours');
+                return duration.humanize();
+              }
+            }
+          }
+        }
+			});
+
+
+    }
+
     renderDailyAverage(data);
     renderLoggedToday(data);
     renderLoggedTime(data);
+    renderCommitsMade(data);
+    renderLinesChanged(data);
   }
 
   return render;
