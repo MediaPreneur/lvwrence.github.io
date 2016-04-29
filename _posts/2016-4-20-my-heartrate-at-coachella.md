2d82f16ebb636de2c346ec140890bbd247e22f97
Lawrence Wu <lawrence@lawrencewu.me>
2016-04-29 13:44:36 -0700

    add coachella data back

--- _posts/2016-4-20-my-heartrate-at-coachella.md
+++ _posts/2016-4-20-my-heartrate-at-coachella.md
@@ -5,14 +5,14 @@ title: My Heartrate at Coachella
 
 I wore my Fitbit to Coachella. Here's the data I scraped from it afterwards.
 Give it some time to load — at 12 samples per minute, there are 17,280 data
-points per graph! Don't look at the code though it's super messy
+points per graph!
 
 <h3>Friday</h3>
-<div id="friday" style="width: 100%; height: 500px;"></div>
+<div id="friday" style="width: 100%; height: 300px;"></div>
 <h3>Saturday</h3>
-<div id="saturday" style="width: 100%; height: 500px;"></div>
+<div id="saturday" style="width: 100%; height: 300px;"></div>
 <h3>Sunday</h3>
-<div id="sunday" style="width: 100%; height: 500px;"></div>
+<div id="sunday" style="width: 100%; height: 300px;"></div>
 
 <script data-main="/public/scripts/coachella"
         src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.2.0/require.min.js"
--- public/scripts/coachella.js
+++ public/scripts/coachella.js
@@ -2,60 +2,97 @@ requirejs.config({
 	paths: {
     superagent: "https://cdnjs.cloudflare.com/ajax/libs/superagent/1.2.0/superagent.min",
     d3: "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.16/d3.min",
-    c3: "https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min"
+    metricsgraphics: "https://cdnjs.cloudflare.com/ajax/libs/metrics-graphics/2.9.0/metricsgraphics.min",
+    jquery: "https://code.jquery.com/jquery-2.2.3.min"
 	},
 });
 
-require(["superagent", "d3", "c3"], function(request, d3, c3) {
+require(["superagent", "d3", "metricsgraphics"], function(request, d3, MG) {
+  // FRIDAY
   request
   .get("https://raw.githubusercontent.com/lvwrence/coachella/master/friday.json")
   .end(function(err, res) {
     var body = JSON.parse(res.text);
     var data = body['activities-heart-intraday']['dataset'];
-    var xs = data.map(function(obj) {
-      return obj['time'];
-    });
-    var hr = data.map(function(obj) {
-      return obj['value'];
+    var data = data.map(function(obj) {
+      var time = moment("2016-04-15 " + obj["time"], "YYYY-MM-DD HH:mm:ss");
+      return {
+        "date": time.toDate(),
+        "value": obj["value"]
+      };
     });
 
-    c3.generate({
-      bindto: '#friday',
-      data: {
-        x: 'x',
-        xFormat: '%H:%M:%S',
-        columns: [
-          ['x'].concat(xs),
-          ['heartrate'].concat(hr),
-        ],
-        type: 'spline'
-      },
-      regions: [
-      ],
-      zoom: {
-        enabled: true
-      },
-      tooltip: {
-        format: {
-          value: function(name, ratio, id, index) {
-            console.log(index);
-          }
-        }
-      },
-      point: {
-        show: false
-      },
-      axis: {
-        x: {
-          type: 'timeseries',
-          tick: {
-            format: '%H:%M'
-          }
-        },
-        y: {
-          label: 'bpm'
-        }
-      }
+    var markers = [{
+        'date': new Date(2016, 3, 15, 3, 0, 0),
+        'label': 'Look, a spike!'
+    }];
+
+    MG.data_graphic({
+        data: data,
+        full_width: true,
+        height: 300,
+        right: 40,
+        markers: markers,
+        target: '#friday'
+    });
+  });
+
+  // SATURDAY
+  request
+  .get("https://raw.githubusercontent.com/lvwrence/coachella/master/saturday.json")
+  .end(function(err, res) {
+    var body = JSON.parse(res.text);
+    var data = body['activities-heart-intraday']['dataset'];
+    var data = data.map(function(obj) {
+      var time = moment("2016-04-16 " + obj["time"], "YYYY-MM-DD HH:mm:ss");
+      return {
+        "date": time.toDate(),
+        "value": obj["value"]
+      };
+    });
+
+    var markers = [{
+        'date': new Date(2016, 3, 16, 3, 0, 0),
+        'label': 'Look, a spike!'
+    }];
+
+    MG.data_graphic({
+        data: data,
+        full_width: true,
+        height: 300,
+        right: 40,
+        markers: markers,
+        target: '#saturday'
+    });
+  });
+
+
+  // SUNDAY
+  request
+  .get("https://raw.githubusercontent.com/lvwrence/coachella/master/sunday.json")
+  .end(function(err, res) {
+    var body = JSON.parse(res.text);
+    var data = body['activities-heart-intraday']['dataset'];
+    var data = data.map(function(obj) {
+      var time = moment("2016-04-17 " + obj["time"], "YYYY-MM-DD HH:mm:ss");
+      return {
+        "date": time.toDate(),
+        "value": obj["value"]
+      };
+    });
+
+    var markers = [{
+        'date': new Date(2016, 3, 17, 3, 0, 0),
+        'label': 'Look, a spike!'
+    }];
+
+    MG.data_graphic({
+        data: data,
+        full_width: true,
+        height: 300,
+        right: 40,
+        markers: markers,
+        target: '#sunday'
     });
   });
 });
