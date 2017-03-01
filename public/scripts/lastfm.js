0138e5179ca48a8f36c75fee8fc72a12eddb3087
Lawrence Wu <lawrence@lawrencewu.me>
2017-03-01 13:32:16 -0500

    add currently listening to

--- public/scripts/lastfm.js
+++ public/scripts/lastfm.js
@@ -1,17 +1,14 @@
 function checkAndSetCurrentlyPlaying() {
-  atomic.get("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Crook--&api_key=e2374a71d02582d469e0604eb87ace84&format=json").success(function (data, xhr) {
-    try {
-      var mostRecentTrack = data["recenttracks"]["track"][0];
-      if (mostRecentTrack["@attr"]["nowplaying"] == "true") {
-        var name = mostRecentTrack["name"];
-        var artist = mostRecentTrack["artist"]["#text"];
-
-        document.getElementById("currently-playing").innerHTML = "currently listening to " + name + " by " + artist;
-      }
-    } catch (e) {
+  db.ref('current_song/lawrence').on('value', function(snapshot) {
+    var currentSong = snapshot.val()
+    var name = currentSong['song']['metadata']['title'];
+    var artist = currentSong['song']['metadata']['artist'];
+    var currentlyListeningTo = "current listening to " + name;
+    if (artist != null) {
+      currentListeningTo += " by " + artist;
     }
-  });
+    document.getElementById("currently-playing").innerHTML = currentlyListeningTo
+  })
 }
 
 checkAndSetCurrentlyPlaying();
-setInterval(checkAndSetCurrentlyPlaying, 1000);
